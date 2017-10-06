e9cdf590c9fb674c34cfc243afe35a73fbec1300
Akiko Yokoyama <acotie.acotie@gmail.com>
2017-10-06 22:34:06 +0900

    add seed data: User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(:email => 'test1@foo.com', :password => '12345678', :password_confirmation => '12345678')
+User.create!(:email => 'test2@foo.com', :password => '12345678', :password_confirmation => '12345678')
+User.create!(:email => 'test3@foo.com', :password => '12345678', :password_confirmation => '12345678')
