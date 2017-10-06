5612fbc801ec1e634c12adeb9031d6b33230fa85
Akiko Yokoyama <acotie.acotie@gmail.com>
2017-10-06 23:13:14 +0900

    add column stripe_customer_id into User Model

---
+++ db/migrate/20171006140822_add_stripe_customer_id_to_users.rb
@@ -0,0 +1,8 @@
+class AddStripeCustomerIdToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :stripe_customer_id, :string
+    add_column :users, :subscribed, :boolean, default: false
+    add_column :users, :subscribed_at, :datetime
+    add_index :users, :stripe_customer_id
+  end
+end
